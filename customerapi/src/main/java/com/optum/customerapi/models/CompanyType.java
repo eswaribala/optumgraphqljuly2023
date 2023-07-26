ec422b088285e7cc7315a4184be8e374aeb65032
eswaribala <parameswaribala@gmail.com>
2023-07-26 14:39:07 +0530

    updated

---
+++ customerapi/src/main/java/com/optum/customerapi/models/CompanyType.java
@@ -0,0 +1,4 @@
+package com.optum.customerapi.models;
+
+public enum CompanyType {
+}
---
+++ customerapi/src/main/java/com/optum/customerapi/models/FullName.java
@@ -0,0 +1,24 @@
+package com.optum.customerapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Embeddable
+public class FullName {
+    @Column(name="First_Name",length = 100,nullable = false)
+    private String firstName;
+    @Column(name="Last_Name",length = 100,nullable = false)
+    private String lastName;
+    @Column(name="Middle_Name",length = 100,nullable = true)
+    private String middleName;
+
+}
