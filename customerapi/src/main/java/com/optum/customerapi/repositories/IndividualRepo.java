99d2d5de09e66957060d332e506677e3ad0711ee
eswaribala <parameswaribala@gmail.com>
2023-07-26 15:10:02 +0530

    updated

--- customerapi/src/main/java/com/optum/customerapi/repositories/IndividualRepo.java
+++ customerapi/src/main/java/com/optum/customerapi/repositories/IndividualRepo.java
@@ -2,6 +2,14 @@ package com.optum.customerapi.repositories;
 
 import com.optum.customerapi.models.Individual;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IndividualRepo extends JpaRepository<Individual,Long> {
+    @Query("Select i from Individual i where i.contactNo=:contactNo")
+    public List<Individual> findByContactNo(@Param("contactNo") long contactNo);
+
+
 }
