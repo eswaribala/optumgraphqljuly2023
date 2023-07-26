83a4a194b55750b51dc477a5f937f25f25248a10
eswaribala <parameswaribala@gmail.com>
2023-07-26 15:21:19 +0530

    updated

--- customerapi/src/main/java/com/optum/customerapi/repositories/AddressRepo.java
+++ customerapi/src/main/java/com/optum/customerapi/repositories/AddressRepo.java
@@ -1,7 +1,15 @@
 package com.optum.customerapi.repositories;
 
 import com.optum.customerapi.models.Address;
+import com.optum.customerapi.models.Individual;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AddressRepo extends JpaRepository<Address,Long> {
+
+    @Query("Select a from Address a where a.customer.customerId=:customerId")
+    public List<Address> findByCustomerId(@Param("customerId") long customerId);
 }
---
+++ customerapi/src/main/java/com/optum/customerapi/services/AddressService.java
@@ -0,0 +1,79 @@
+package com.optum.customerapi.services;
+
+import com.optum.customerapi.models.Address;
+import com.optum.customerapi.models.Customer;
+import com.optum.customerapi.repositories.AddressRepo;
+import com.optum.customerapi.repositories.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.AbstractQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Service
+public class AddressService {
+    @Autowired
+    private AddressRepo addressRepo;
+    @Autowired
+    private CustomerRepo customerRepo;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    //insert
+
+    public Address addAddress(Address address,long customerId){
+
+        Customer customer=this.customerRepo.findById(customerId).orElse(null);
+        if(customer!=null){
+            address.setCustomer(customer);
+            return this.addressRepo.save(address);
+        }
+        else
+            return null;
+
+
+    }
+
+    //select all
+
+    public List<Address> getAllAddresss(){
+        return this.addressRepo.findAll();
+    }
+
+    //select by id
+
+    public List<Address> getAddressById(long customerId){
+        return this.addressRepo.findByCustomerId(customerId);
+    }
+
+
+
+
+    //update
+
+    public Address updateAddress(Address address,long customerId){
+        Customer customer=this.customerRepo.findById(customerId).orElse(null);
+        if(customer!=null){
+            address.setCustomer(customer);
+            return this.addressRepo.save(address);
+        }
+        else
+            return null;
+    }
+
+
+    //delete
+    public boolean deleteAddress(long addressId){
+        boolean status=false;
+        this.addressRepo.deleteById(addressId);
+        status=true;
+
+        return status;
+    }
+}
