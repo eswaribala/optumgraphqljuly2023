eb7c65e82ac2a87d048d6b8736ff8c830bc2a081
eswaribala <parameswaribala@gmail.com>
2023-07-26 14:45:11 +0530

    updated

---
+++ customerapi/src/main/java/com/optum/customerapi/models/Address.java
@@ -0,0 +1,31 @@
+package com.optum.customerapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Address")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="Address_Id")
+    private long addressId;
+    @Column(name="Door_No",length = 5,nullable = false)
+    private String doorNo;
+    @Column(name="Street_Name",length = 100,nullable = false)
+    private String streetName;
+    @Column(name="City",length = 100,nullable = false)
+    private String city;
+    @Column(name="Pincode")
+    private long pincode;
+    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.MERGE)
+    @JoinColumn(foreignKey = @ForeignKey(name = "Customer_Id"), name = "Customer_Id")
+    private Customer customer;
+
+}
