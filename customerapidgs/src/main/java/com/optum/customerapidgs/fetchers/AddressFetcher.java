011f02dea68649171d4a0193fb50ac5e6cb3646e
eswaribala <parameswaribala@gmail.com>
2023-07-28 16:28:58 +0530

    updated

--- customerapidgs/src/main/java/com/optum/customerapidgs/fetchers/AddressFetcher.java
+++ customerapidgs/src/main/java/com/optum/customerapidgs/fetchers/AddressFetcher.java
@@ -6,7 +6,10 @@ import com.netflix.graphql.dgs.DgsQuery;
 import com.optum.customerapidgs.dtos.AddressInput;
 import com.optum.customerapidgs.exceptions.NoDataFoundError;
 import com.optum.customerapidgs.models.Address;
+import com.optum.customerapidgs.models.Customer;
 import com.optum.customerapidgs.repositories.AddressRepo;
+import com.optum.customerapidgs.repositories.CustomerRepo;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -15,6 +18,9 @@ import java.util.List;
 public class AddressFetcher {
     @Autowired
     private AddressRepo addressRepo;
+
+    @Autowired
+    private CustomerRepo customerRepo;
 
     @DgsQuery
     public List<Address> findAllAddresses(){
@@ -33,14 +39,34 @@ public class AddressFetcher {
 
     }
 
-
+@DgsMutation
     public Address  addAddress(AddressInput addressInput, long customerId){
+    return getAddress(addressInput);
+
+}
+
+    public Address updateAddress(long customerId, AddressInput addressInput){
+        return getAddress(addressInput);
 
     }
 
-    public Address updateAddress(long customerId, AddressInput addressInput){
+    @Nullable
+    private Address getAddress(AddressInput addressInput) {
+        Customer customer=this.customerRepo.findById(addressInput.getCustomer().getCustomerId()).orElse(null);
 
-
+        if(customer!=null) {
+            Address address = Address.builder()
+                    .addressId(addressInput.getAddressId())
+                    .city(addressInput.getCity())
+                    .doorNo(addressInput.getDoorNo())
+                    .streetName(addressInput.getStreetName())
+                    .pincode(addressInput.getPincode())
+                    .customer(customer)
+                    .build();
+            return this.addressRepo.save(address);
+        }else {
+            return null;
+        }
     }
 
     @DgsMutation
